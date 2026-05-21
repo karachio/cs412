1a873ef6751e4938de6c0d7991caf229ed7eb17b
Karachi Onwuanibe <karachio@bu.edu>
2026-05-21 13:20:15 -0400

    changed some lines of code

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from django.conf import settings
 from . import views
- 
- 
-urlpatterns = [ 
-    path(r'', views.main, name="restaurant"),
-    path(r'main', views.main, name="main"), 
-    path(r'order', views.order, name="order"), 
-    path(r'confirmation', views.confirmation, name="confirmation"), 
-    
+
+urlpatterns = [
+    path('', views.main, name="restaurant"),
+    path('main/', views.main, name="main"),
+    path('order/', views.order, name="order"),
+    path('confirmation/', views.confirmation, name="confirmation"),
 ]
