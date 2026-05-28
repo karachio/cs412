52609d42e424a48bb701fd381afbec6a9b8ba718
Karachi Onwuanibe <karachio@bu.edu>
2026-05-28 13:55:25 -0400

    fixed deployment issues.

--- cs412/settings.py
+++ cs412/settings.py
@@ -131,12 +131,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL= "/media/"
+
 import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
-#if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
- #   STATIC_URL = '/karachio/static/'
-  #  MEDIA_URL = '/karachio/media/'
+if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
+    STATIC_URL = '/karachio/static/'
+    MEDIA_URL = '/karachio/media/'
     
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL= "/media/"
